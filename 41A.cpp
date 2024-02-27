9d5ea1edd3d3bdb3733a6845228a0707d9ae55d0
Mahmoud Hussain <121444044+Mahmoud-Hussain@users.noreply.github.com>
2024-02-27 21:47:36 +0600

    update

---
+++ 41A.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string t,s;
+    int ans=1;
+    cin>>t>>s;
+    if(t.length()==s.length()){
+        for(int i=0;i<t.length();i++){
+            if(t[i]!=s[t.length()-i-1]){
+                ans=0;
+                break;
+            }
+        }
+    }
+    else ans=0;
+    if(ans)cout<<"YES";
+    else cout<<"NO";
+    return 0;
+}
